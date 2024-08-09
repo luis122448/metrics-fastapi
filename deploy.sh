d2b008e7881d80ba4b98acdf05cbaca9fdbb9483
LUIS ANTONIO CALVO QUISPE <luis122448@gmail.com>
2024-08-09 00:20:06 -0500

    deploy(app): add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# Description: Deploy the application
+
+# Stop the application
+sudo docker compose down
+
+# Pull the latest changes
+sudo git pull origin main
+
+# Build and run the application
+sudo docker compose up --build --force-recreate --no-deps -d
